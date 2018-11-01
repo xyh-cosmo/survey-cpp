c5b633bed08e7dcf956dbeabb7f20607a07ecf96
xyh-cosmo <yhxu@nao.cas.cn>
2018-11-01 19:45:17 +0800

    add contents into include/CSS_configure.hpp src/CSS_configure.cpp

--- include/CSS_configure.hpp
+++ include/CSS_configure.hpp
@@ -1,12 +1,40 @@
 //	This class holds all the relevant configuration parameters that are needed
-//	to perform the survey simulation
+//	to perform the survey simulation. This class will be called by ALL other
+//  modules to get parameters to be used!
+//
+//  Created @ 2018-11-1
+//  Author: YOUHUA XU
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+#include "dictionary.h"
+#include "iniparser.h"
 
 #ifndef _CSS_CONFIGURE_HPP_
 #define _CSS_CONFIGURE_HPP_
 
 class CSS_Configure{
+    dictionary *ini_dict;
+public:
 
+//  configuration parameters:
+    double survey_start_time;
+    double survey_end_time;
+
+    CSS_Configure();
+    ~CSS_Configure();
+
+    void read_configure( std::string& config_file_name );
+    void save_configure( std::string& config_file_name );
+
+    int get_int( std::string& key );
+    double get_double( std::string& key );
+    std::string get_string( std::string& key );
 };
 
 
---
+++ src/CSS_configure.cpp
@@ -0,0 +1,9 @@
+#include "CSS.hpp"
+
+CSS_Configure::CSS_Configure(){
+
+}
+
+CSS_Configure::~CSS_Configure(){
+    
+}
