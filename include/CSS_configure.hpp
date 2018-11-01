5a0972888e791699dbc20185ebc1b06d7eac0ecb
xyh-cosmo <yhxu@nao.cas.cn>
2018-11-01 19:26:52 +0800

    create class CSS_Configure

---
+++ include/CSS_configure.hpp
@@ -0,0 +1,13 @@
+//	This class holds all the relevant configuration parameters that are needed
+//	to perform the survey simulation
+
+
+#ifndef _CSS_CONFIGURE_HPP_
+#define _CSS_CONFIGURE_HPP_
+
+class CSS_Configure{
+
+};
+
+
+#endif //_CSS_CONFIGURE_HPP_
