9e7acdcf2716080d8b40990beba5b9d9d3803437
cosmo-xyh <yhxu@nao.cas.cn>
2018-11-01 23:14:19 +0800

    remove old job scripts

--- job-scripts/debug-0714/sub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-qsub -q i0.q -pe mpich 12 B18_E20.job  
-qsub -q i1.q -pe mpich 12 B20_E18.job
-qsub -q i2.q -pe mpich 12 B20_E20.job  
-qsub -q i3.q -pe mpich 12 B20_E22.job  
-qsub -q i4.q -pe mpich 12 B22_E20.job  
-qsub -q i5.q -pe mpich 12 B22_E22.job
--- job-scripts/debug-0730/sub.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-qsub -q x120.q -pe mpich 48 E19_b17_beta_10.25.job-A
-qsub -q x120.q -pe mpich 48 E19_b17_beta_10.25.job-B
-
